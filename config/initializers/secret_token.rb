79763f38490de80ebea0a6b7f39969c15ba6767e
Jordan Brown <jordanfbrown@gmail.com>
2013-04-14 17:55:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personal::Application.config.secret_token = '6591098dbc310a9727b999a536a1b4de000f057ea75f12bda346589170b1dae1bb09871e34e75f70a3678679441ce57d07d76c60421584aa441fa096237a6e8a'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Personal::Application.routes.draw do
+  root :to => 'pages#home'
+end
